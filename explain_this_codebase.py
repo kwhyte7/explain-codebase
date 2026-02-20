ff007b70d3aab911d04232a2d2de561219a4d3d1
Kingston Whyte <kingstonwhyte7@gmail.com>
2026-02-20 17:14:51 +0000

    aider don't cuck this up

--- explain_this_codebase.py
+++ explain_this_codebase.py
@@ -15,17 +15,19 @@ args = parser.parse_args()
 model = args.model or "qwen3:4b"
 directory = args.directory = os.getcwd()
 
-print(model, directory)
+def relative_path(target, relative_to): # this could be improved, AI!
+    return target.replace(relative_to, "")
 
 # read gitignore, then ignore matching files
-
-# argv is model, directory
+def prune_gitignore_and_common(files_list: list)
+    # list[str]
+    
+    # I want this function to: read .gitignore, prune those files (also prune hidden files/folders), AI!
 
 # scan for files
-def scan_for_files(directory="./"):
-    pass
-
-# use glob for tree
-
-# ignore hidden files
-
+def scan_for_text_files(directory="./"):
+    files_to_evaluate = glob(os.path.join(directory, "*"))
+    # accept .py, .js, ETC AI!
+    
+    return tree_to_evaluate(files_to_evaluate)
+    
